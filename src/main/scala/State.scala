f7cf00c3b94a22dcf84196b7b20db57d7a985223
Misterturtle <Ryan@conaway58.com>
2017-06-23 09:47:41 -0400

    Refactored State system. Added tests involving transitioning from WorldMapState to InitPlaceState

--- src/main/scala/State.scala
+++ src/main/scala/State.scala
@@ -1,13 +1,61 @@
 /**
   * Created by Harambe on 6/16/2017.
   */
-case class Transition(conditions: List[() => Boolean], effects: List[() => Unit], newStateRef: () => State, popOldState: Boolean)
+trait Transition{
+  val conditions: List[() => Boolean]
+}
+case class ForwardTransition(conditions: List[() => Boolean], forwardTransState: State) extends Transition
+case class ReturnTransition(conditions: List[() => Boolean]) extends Transition
 
-case class State(transitions:List[Transition] = Nil) {
+abstract class State() {
 
-  def update(): Option[Transition] = transitions.find(_.conditions.forall(_()))
+  protected var _forwardState: Option[State] = None
+  protected var _returnState:Boolean = false
+  def forwardState: Option[State] = _forwardState
 
-  def registerTransition(newTransition: Transition): State = copy(newTransition :: transitions)
+  val transitions: List[Transition]
+
+  def returnState:Boolean = _returnState
+
+  def update(): Unit = {
+    checkReactivation()
+    _forwardState match {
+      case Some(state) =>
+        state.update()
+
+      case None =>
+        checkTransitions()
+    }
+  }
+
+  private def checkReactivation(): Unit ={
+    if(_forwardState.exists(_.returnState))
+      _forwardState = None
+  }
+
+  private def checkTransitions(): Unit ={
+    transitions.find(_.conditions.forall(_())) match {
+      case Some(forwardTrans:ForwardTransition) =>
+        _forwardState = Some(forwardTrans.forwardTransState)
+
+      case Some(returnTrans:ReturnTransition) =>
+        _returnState = true
+
+      case None =>
+    }
+  }
+}
 
 
+class TestState(val transitions:List[Transition]) extends State()
+
+case class WorldMapState(initPlaceComp: () => Boolean) extends State(){
+
+
+  val transToInitPlace = ForwardTransition(List(() => !initPlaceComp()), InitPlaceState())
+  val transitions = List[Transition](transToInitPlace)
+}
+
+case class InitPlaceState() extends State{
+  override val transitions: List[Transition] = Nil
 }
